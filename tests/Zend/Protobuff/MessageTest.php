babac8324bd59fcf2d5b3dc26ce7d6cdbf4bbfeb
Rostislav Mykhajliw <necromant2005@gmail.com>
2010-12-12 14:52:22 +0200

    sceleton

---
+++ tests/Zend/Protobuff/MessageTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Test;
+
+class MessageTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInit()
+    {
+
+    }
+
+    public function testEncode()
+    {
+
+    }
+
+    public function testDecode()
+    {
+
+    }
+}
+
---
+++ tests/bootstrap.php
@@ -0,0 +1,62 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(__DIR__ . '/../library'),
+    get_include_path(),
+)));
+
+function ZendTest_Autoloader($class)
+{
+    $class = ltrim($class, '\\');
+
+    if (!preg_match('#^(Zend(Test)?|PHPUnit)(\\\\|_)#', $class)) {
+        return false;
+    }
+
+    // $segments = explode('\\', $class); // preg_split('#\\\\|_#', $class);//
+    $segments = preg_split('#[\\\\_]#', $class); // preg_split('#\\\\|_#', $class);//
+    $ns       = array_shift($segments);
+    if ($ns=='PHPUnit') return false;
+
+    $segment = $segments[0];
+
+    switch ($ns) {
+        case 'Zend':
+            if ($segment=='Db\\Document') {
+                $file = dirname(__DIR__) . '/library/Zend/Protobuff/';
+                array_shift($segments);
+                break;
+            }
+            $file = 'Zend/';
+            break;
+        default:
+            $file = false;
+            break;
+    }
+
+    if ($file) {
+        $file .= implode('/', $segments) . '.php';
+        if ($file[0]=='/') {
+            var_dump($file);
+            if (file_exists($file)) {
+                return include_once $file;
+            }
+        } else {
+            foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+                $pathname = realpath($path) . '/' . $file;
+                var_dump($pathname);
+                if (file_exists($pathname)) {
+                    return include_once $pathname;
+                }
+            }
+        }
+    }
+    return false;
+}
+spl_autoload_register('ZendTest_Autoloader', true, true);
+
+require_once __DIR__ . '/configuration.php';
+
